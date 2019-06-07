2fdb79304ce42acaa13e712e2afb52255dda1d8b
Farhad Faramarzi <farhad.public@gmail.com>
2019-06-07 11:41:34 +0430

    Update: add navaigation

--- App.js
+++ App.js
@@ -1,11 +1,17 @@
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
 
-import {  } from 'expo';
+import { AppLoading, Asset } from 'expo'
+
+import Navigation from './navigation'
+import { Block } from './components'
+import * as constants from './constants'
+
 export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
+        <Navigation />
       </View>
     );
   }
